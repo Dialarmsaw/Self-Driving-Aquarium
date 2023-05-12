7748914d5b73e8e03d0a76a563a364ed83563bcd
Dialarmsaw <83833689+Dialarmsaw@users.noreply.github.com>
2023-05-12 11:16:48 -0400

    2D&3D_Driving.py

--- drive.py
+++ drive.py
@@ -8,7 +8,7 @@ import serial
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 ser.reset_input_buffer()
 
-##########DEFS##########
+##########CAMERA##########
 def callback(value):
     pass
 
@@ -49,9 +49,15 @@ def showWindows(frame1, frame2, mask1, mask2):
     cv2.imshow('video2', frame2)
     cv2.imshow("mask2", mask2)
 
-def calculations(x, y):
+######MATH########
+def scaleFactor(num, factor):
+    num = num/factor
+    return num
+
+def calc2D(x, y):
     #A, D equation: sin(angle+1/4pi)*mag
     #B, C equation: sin(angle-1/4pi)*mag
+    #This treats the XY as if it is a joystick
     angle =  math.atan2(y, x)
     mag = math.sqrt(x**2 + y**2)
     pi = math.pi
@@ -59,8 +65,22 @@ def calculations(x, y):
     out2 = math.sin(angle-(pi/4))*mag
     return out1, out2
 
+def calc3D(x, y, z):
+    #A, D equation: sin(angle+1/4pi)*mag
+    #B, C equation: sin(angle-1/4pi)*mag
+    #This treats it like a 2d joystick, except ew are ignoring the the magnitude. The magnitude
+    #is from the height of the fish
+    offset = 0
+    angle = math.atan2(y, x)
+    mag = z - offset
+    pi = math.pi
+    out1 = math.sin(angle+(pi/4))*mag
+    out2 = math.sin(angle-(pi/4))*mag
+    return out1, out2
+
 def writeOut(out1, out2):
     ser.write(b"{}, {}\n".format(out1, out2))
+
 #Setup Cameras
 width=720
 height=480
@@ -82,9 +102,9 @@ while True:
     _, frame2 = cam2.read() #back camera (LR, UD)
     #get the XY's of the fish (adding z soon)
     y, p, mask1 = getXY(frame1) #y is the x axis of the right camera
-    x, q, mask2= getXY(frame2) #x is the x axis of the bottom camera
+    x, z, mask2= getXY(frame2) #x is the x axis of the bottom camera
     
-    out1, out2 = calculate(x/960, y/960) #this assumes the cameras are perfectly square, fix this
+    out1, out2 = calc2D(x/960, y/960) #this assumes the cameras are perfectly square, fix this
     
     writeOut(out1, out2)
     
